50a49f76320e19083aaa6de1dbd04080c3ecb943
Mkang1204 <mengjiakang2017@u.northwestern.edu>
2019-02-15 17:02:13 -0600

    Update querying.sql

--- mimic_db/querying.sql
+++ mimic_db/querying.sql
@@ -1,3 +1,26 @@
+-- Step 1
+-- First start with retrieving ‘subject_id’, ‘hadm_id’, ‘icustay_id’, ‘intime’, and ‘outtime’ from the ‘icustays’ table.
+
+-- Step 2
+-- Using the patients table retrieve the calculated age of patients.
+
+-- Step 3
+-- Separate neonates from adult patients.
+
+-- Step 4
+-- By incorporating the admissions table, find how long each stay was BEFORE the patients were admitted to the ICU
+
+-- Step 5
+-- Next find the date of the patient’s death if applicable.
+
+-- Step 6
+-- Then find those deaths that occurred while the patients were in the hospital.
+
+-- Step 7
+-- Find how many of those deaths occurred within the ICU.
+
+
+
 SELECT row_id, subject_id, hadm_id, admittime, dischtime, deathtime, admission_type, admission_location, discharge_location, insurance, language, religion, marital_status, ethnicity, edregtime, edouttime, diagnosis, hospital_expire_flag, has_chartevents_data
 	FROM mimiciii.admissions;
 SELECT row_id, subject_id, gender, dob, dod, dod_hosp, dod_ssn, expire_flag
