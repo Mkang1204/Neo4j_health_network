b1ac54d1ea5a39abd8ff5002781b66e29df1e3ca
Mkang1204 <mengjiakang2017@u.northwestern.edu>
2019-04-25 11:37:05 -0500

    deleted useless

--- Dairy/leetcode/maxDepthTree.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        height = 0
-        if root == None:
-            return height
-        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))        
-    # def maxHeight(self, root.left, root.right):
-        
